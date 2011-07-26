39d5ad802e489d80d63ef654c109c7f9327bd065
田生 <tiansh@users.noreply.github.com>
2011-07-26 07:05:12 +0000

    0.1

--- js/dn.js
+++ js/dn.js
@@ -1,9 +1,36 @@
+/*
+    This file is part of Tianjin Mahjong.
+
+    Tianjin Mahjong is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // 电脑打牌——选择
 var dap_dn=function () {
   var s=sp[zpos];
   if (s.pdhp(chty===2).ky) {
     callfunc(hup);
   } else {
+    // 判断暗杠和小明杠
+    var i,j,c;
+    for (i=0;i<14;i++) if(!s[i].samehuir()) {
+      for (j=i,c=0;j<14;c+=(s[j++].same(s[i]))?1:0);
+      if (c===4) if (dnag(s,s[i])) {
+        dapc=new zhang(s[i].lb,s[i].sz);
+        callfunc(xm_agang);
+        return;
+      }
+    }
     var d=comchose(s);
     dapc=new zhang(d.lb,d.sz);
     callfunc(dac); 
@@ -12,5 +39,12 @@ var dap_dn=function () {
 
 // 电脑选择碰杠
 var pg_dn=function () {
-  callfunc(peng);
+  var s=sp[ppos];
+  var h=dnpg(s,dapc);
+  var i, c;
+  for (c=0,i=0;i<s.pz();i++)
+    if (s[i].same(dapc)) c++;
+  if (c===3 && h==="g") callfunc(dmgang);
+  else if (h==="p") callfunc(peng);
+  else callfunc(pgqx);
 }
--- js/wj.js
+++ js/wj.js
@@ -1,3 +1,20 @@
+/*
+    This file is part of Tianjin Mahjong.
+
+    Tianjin Mahjong is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // 玩家打牌——选择
 // 对每张牌注册onclick事件
 var dap_wj_ro=function () {
@@ -11,6 +28,7 @@ var dap_wj_co=function () {
 }
 // 初始化打出牌的界面
 var dap_wj=function () {
+  
   if (chty!==3) {
     // 判断是否能杠牌并在按钮的列表中加入杠牌的按钮
     var i,j,c,s=sp[3],l=Array(3),n=0,b=[];
@@ -19,11 +37,16 @@ var dap_wj=function () {
       if (c===4) l[n++]=new zhang(s[i].lb,s[i].sz);
     }
     // 如果只有一种牌能杠，则显示按钮为“杠牌”字样
-    if (n===1) b[b.length]={n:"杠牌",f:xm_ag_wj_i,v:l[0].divtag().className};
+    if (n===1) b[b.length]={
+      n:"杠牌",
+      f:xm_ag_wj_i,v:
+      l[0].divtag().className
+    };
     // 否则显示为“杠五万”，“杠东字”等字样
     else for (i=0;i<n;i++)
       b[b.length]={
-        n:"杠"+(l[i].lb!==0?
+        n:"杠"+(
+        l[i].lb!==0?
           (ZH_SZ[l[i].sz]+ZH_LB[l[i].lb]):
           (ZH_FJ[l[i].sz])
         ),
@@ -31,13 +54,14 @@ var dap_wj=function () {
         v:l[i].divtag().className
       }
     // 判断是否能和牌  
-    if (s.pdhp(chty===2).ky) b[b.length]={n:"和牌",f:hup_wj_i};
+    if (s.pdhp(chty===2).ky)
+     b[b.length]={n:"和牌",f:hup_wj_i};
     if (b.length>0) setButtons(b);
   }
   comchose(sp[3]);
   dap_wj_ro();
 }
-// 接收用户小明杠的动作并继续
+// 接收用户暗杠和小明杠的动作并继续
 var xm_ag_wj_i=function () {
   clearButtons();
   dap_wj_co();
@@ -66,7 +90,8 @@ var peng_wj_i=function () {
 var dmgang_wj_i=function () {
   clearButtons();
   callfunc(dmgang);
-}// 接收用户取消碰杠的动作并继续
+}
+// 接收用户取消碰杠的动作并继续
 var pgqx_wj_i=function () {
   clearButtons();
   callfunc(pgqx);
